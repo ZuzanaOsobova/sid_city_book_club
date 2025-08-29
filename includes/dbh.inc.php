f98cac8eee89067c098eeb7338b8a25af813e102
Zuzana Osobová <osoz00@vse.cz>
2025-08-29 20:45:20 +0200

    basics for php working with database, selecting and query of statements

---
+++ includes/dbh.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+$dbh = "mysql:host=localhost;dbname=sid_city_book_club";
+
+
+//jméno a heslo, tyhle se mění na základě databáze
+$dbUsername = "root";
+$dbPassword = "";
+
+
+try {
+    $pdo = new PDO($dbh, $dbUsername, $dbPassword);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connection successful";
+} catch (PDOException $e) {
+
+
+    // Print an error message to indicate failure to connect to DB
+    echo "Connection failed: " . $e->getMessage();
+}
